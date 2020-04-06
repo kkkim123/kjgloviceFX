b2eaae0676fd6ea9db6c64e268c51c9ebd6632f3
cksdud3037 <cksdud@naver.com>
2020-04-06 18:44:42 +0900

    url path add

--- restAPI/frontend/urls.py
+++ restAPI/frontend/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index),
     path('login', views.index),
     re_path('register/', views.index),
+    path('rest', views.index),
+    path('resetConfirm', views.index),
     path('trading', views.index),
-
 ]
