2d533184af91151e250a3e19570a8e5310612389
cksdud3037 <cksdud@naver.com>
2020-05-06 11:35:26 +0900

    오류 수정, 재빌드

--- restAPI/frontend/src/components/mypage/components/overview.js
+++ restAPI/frontend/src/components/mypage/components/overview.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import store from '../../../store';
 import { getOverview } from '../../../actions/mypage';
+import Moment from 'moment';
 
 
 class Overview extends Component {
@@ -39,39 +40,39 @@ class Overview extends Component {
                 <br></br>
                     <div className="d-flex justify-content-between" style={{ borderTop: "1px solid #000000", color: "#929292", fontSize:"1.2rem", padding:"0.8rem"}}>
                         <div className="ml-1"><span>Balance:</span></div>
-                        <div className="text-center" ><span>{this.props.info ? this.props.info.balance : "-"}</span></div>
+                        <div className="text-center" ><span>{this.props.info && this.props.info.balance ? this.props.info.balance.toFixed(2) : "-"}</span></div>
                     </div>
                     <div className="d-flex justify-content-between" style={{ borderTop: "1px solid #000000", color: "#929292", fontSize:"1.2rem", padding:"0.8rem"}}>
                         <div className="ml-1"><span>Equity:</span></div>
-                        <div className="text-center" ><span>{this.props.info && this.props.info.equity ? this.props.info.equity.toFixed(3) : "-"}</span></div>
+                        <div className="text-center" ><span>{this.props.info && this.props.info.equity ? this.props.info.equity.toFixed(2) : "-"}</span></div>
                     </div>
                     <div className="d-flex justify-content-between" style={{ borderTop: "1px solid #000000", color: "#929292", fontSize:"1.2rem", padding:"0.8rem"}}>
                         <div className="ml-1"><span>Floating P/L:</span></div>
-                        <div className="text-center" ><span>{this.props.info && this.props.info.floating_profit ? this.props.info.floating_profit.toFixed(3) : "-"}</span></div>
+                        <div className="text-center" ><span>{this.props.info && this.props.info.floating_profit ? this.props.info.floating_profit.toFixed(2) : "-"}</span></div>
                     </div>
                     <div className="d-flex justify-content-between" style={{ borderTop: "1px solid #000000", color: "#929292", fontSize:"1.2rem", padding:"0.8rem"}}>
                         <div className="ml-1"><span>Closed profit:</span></div>
-                        <div className="text-center" ><span>{this.props.info? this.props.info.closedProfit : "-"}</span></div>
+                        <div className="text-center" ><span>{this.props.info && this.props.info.closed_profit ? this.props.info.closed_profit.toFixed(2) : "-"}</span></div>
                     </div>
                     <div className="d-flex justify-content-between" style={{ borderTop: "1px solid #000000", color: "#929292", fontSize:"1.2rem", padding:"0.8rem"}}>
                         <div className="ml-1"><span>Free margin:</span></div>
-                        <div className="text-center" ><span>{this.props.info? this.props.info.freeMargin : "-"}</span></div>
+                        <div className="text-center" ><span>{this.props.info && this.props.info.margin_free ? this.props.info.margin_free.toFixed(2) : "-"}</span></div>
                     </div>
                     <div className="d-flex justify-content-between" style={{ borderTop: "1px solid #000000", color: "#929292", fontSize:"1.2rem", padding:"0.8rem"}}>
                         <div className="ml-1"><span>Margin in use:</span></div>
-                        <div className="text-center" ><span>{this.props.info? this.props.info.margin : "-"}</span></div>
+                        <div className="text-center" ><span>{this.props.info && this.props.info.margin ? this.props.info.margin.toFixed(2) : "-"}</span></div>
                     </div>
                     <div className="d-flex justify-content-between" style={{ borderTop: "1px solid #000000", color: "#929292", fontSize:"1.2rem", padding:"0.8rem"}}>
                         <div className="ml-1"><span>Margin Level:</span></div>
-                        <div className="text-center" ><span>{this.props.info && this.props.info.margin_level ? this.props.info.margin_level.toFixed(3) : "-"}</span></div>
+                        <div className="text-center" ><span>{this.props.info && this.props.info.margin_level ? this.props.info.margin_level.toFixed(2) + '%' : "-"}</span></div>
                     </div>
                     <div className="d-flex justify-content-between" style={{ borderTop: "1px solid #000000", color: "#929292", fontSize:"1.2rem", padding:"0.8rem"}}>
                         <div className="ml-1"><span>Leverage:</span></div>
-                        <div className="text-center" ><span>{this.props.info ? "1:"+this.props.info.leverage : "-"}</span></div>
+                        <div className="text-center" ><span>{this.props.info ? "1 : "+this.props.info.leverage : "-"}</span></div>
                     </div>
                     <div className="d-flex justify-content-between" style={{ borderTop: "1px solid #000000", color: "#929292", fontSize:"1.2rem", padding:"0.8rem"}}>
                         <div className="ml-1"><span>Last Update:</span></div>
-                        <div className="text-center" ><span>{this.props.info? this.props.info.lastUpdate : "-"}</span></div>
+                        <div className="text-center" ><span>{this.props.info && this.props.info.last_update ? Moment(this.props.info.last_update).format("DD/MM/YYYY HH:mm:ss") : "-"}</span></div>
                     </div>
                 
             </div>
